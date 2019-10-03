ad21eeeb96a3ba333814a16bd19227b944b16c40
liatmoss <liatmoss@hotmail.com>
2019-10-03 13:45:03 +0100

    final spec code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
-require './lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 10' do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 17' do
+    expect(fizzbuzz(17)).to eq 17
   end
 end
